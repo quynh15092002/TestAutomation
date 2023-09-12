c1c9898ac2bb5d28e3c48715590a7e6aee04958f
hoangthong1909 <hoangthong19092002@gmail.com>
2023-09-12 09:47:28 +0700

    Check automation test search

---
+++ src/main/java/com/selenium/test/Check1.java
@@ -0,0 +1,18 @@
+package com.selenium.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Check1 {
+    public static void main(String[] arStrings) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("Hello Selenium Webdriver");
+        element.submit();
+        System.out.println("Page title is: " + driver.getTitle());
+    }
+}
