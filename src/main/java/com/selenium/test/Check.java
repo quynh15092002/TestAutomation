0317274a70e6f2545f99bfb9ef9db0c65df48f38
QuynhNguyen <quynh.nguyen@tomosia.com>
2023-09-12 16:02:33 +0700

    Check automation test search

--- src/main/java/com/selenium/test/Check.java
+++ src/main/java/com/selenium/test/Check.java
@@ -1,2 +1,17 @@
-package com.selenium.test;public class Check {
+package com.selenium.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Check {
+    public static void main(String[] arStrings) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("Hello Selenium Webdriver");
+        element.submit();
+        System.out.println("Page title is: " + driver.getTitle());
+    }
 }
